0fa16b24d517d3a04b0bd075fa5da763f4167ee2
Marius Vlas <marius.vlas@rodeapps.com>
2017-09-04 03:15:28 +0300

    Done Navigation Scrollbar

--- src/components/main_page/MainPage.js
+++ src/components/main_page/MainPage.js
@@ -2,7 +2,7 @@
  * Created on 03/9/17.
  */
 import React, {Component} from 'react';
-
+import {scroller} from 'react-scroll';
 class MainPage extends Component {
 
     constructor() {
@@ -48,23 +48,75 @@ class MainPage extends Component {
             'title': null,
             'icon': null
         }];
+        this.navigationElements = [];
+
+        this.handleScrollDrag = this.handleScrollDrag.bind(this);
     }
 
     setCardsPosition() {
         for (let i = 0; i < this.cards.length; i++) {
             this.cards[i].container.style.transform = 'translateY(' + -(document.body.scrollTop / 4) + 'px) translateZ(0)';
 
-            const titleMin = this.cards[i].container.getBoundingClientRect().top / 10;
-            const iconMin = this.cards[i].container.getBoundingClientRect().top / 4;
-            this.cards[i].title.style.transform = 'translateY(' + titleMin + 'px) translateZ(0)';
-            this.cards[i].icon.style.transform = 'translateY(' + iconMin + 'px) translateZ(0)';
+            const titleTranslate = Math.max(0, Math.min(this.cards[i].container.getBoundingClientRect().top / 12, 190));
+            const iconTranslate = Math.max(0, Math.min(this.cards[i].container.getBoundingClientRect().top / 5, 190));
+            this.cards[i].title.style.transform = 'translateY(' + titleTranslate + 'px) translateZ(0)';
+            this.cards[i].icon.style.transform = 'translateY(' + iconTranslate + 'px) translateZ(0)';
+        }
+    }
 
+    highlightNavigationLabels() {
+        const barTop = this.navigationCurrentBar.getBoundingClientRect().top;
+        const barHeight = this.navigationCurrentBar.offsetHeight;
+
+        for (let i = 0; i < this.navigationElements.length; i++) {
+            const labelElement = this.navigationElements[i];
+
+            if(labelElement.getBoundingClientRect().top + 20 > barTop && labelElement.getBoundingClientRect().top + 40 < barTop + barHeight) {
+                labelElement.classList.add('highlighted');
+            }
+            else {
+                labelElement.classList.remove('highlighted');
+            }
         }
     }
 
     setNavigationPosition() {
-        // this.navigationCurrentBar.style.top = (document.body.scrollTop / document.body.scrollHeight * 100) + '%';
-        // this.navigationCurrentBar.style.height = (document.body.offsetHeight / document.body.scrollHeight * 100) + '%';
+        this.navigationCurrentBar.style.top = (document.body.scrollTop / document.body.scrollHeight * 100) + '%';
+        this.navigationCurrentBar.style.height = (document.body.offsetHeight / document.body.scrollHeight * 100) + '%';
+        const bodyHeight = document.body.scrollHeight;
+        for (let i = 0; i < this.cards.length; i++) {
+            this.navigationElements[i].style.top = (this.cards[i].container.offsetTop - document.body.scrollTop / 4) / bodyHeight * 100 + '%';
+        }
+
+        this.highlightNavigationLabels();
+    }
+
+
+    scrollToCard(index) {
+        const clientRect = this.cards[index].container.getBoundingClientRect().top;
+        if (clientRect < 0 || clientRect > document.body.offsetHeight - 600) {
+
+            scroller.scrollTo(`card-${this.state.cardsList[index].title}`, {
+                duration: 600,
+                delay: 50,
+                smooth: true,
+                containerId: 'ContainerElementID',
+                offset: -(clientRect + 336) / 4
+            })
+        }
+    }
+
+    handleScrollDrag(event) {
+        if (event.stopPropagation) {
+            event.stopPropagation();
+        }
+        if (event.preventDefault) {
+            event.preventDefault();
+        }
+
+        const heightsRatio = document.body.scrollHeight / this.navigationContainer.offsetHeight;
+        const yDiff = event.screenY - this.initialPageY;
+        window.scrollTo(0, this.initialScroll + yDiff * heightsRatio);
     }
 
     componentDidMount() {
@@ -76,8 +128,20 @@ class MainPage extends Component {
             this.setNavigationPosition();
         });
         window.addEventListener('resize', () => {
-            // this.navigationCurrentBar.style.height = (document.body.offsetHeight / document.body.scrollHeight * 100) + '%';
-        })
+            this.setNavigationPosition();
+        });
+
+
+        this.navigationCurrentBar.addEventListener('mousedown', (event) => {
+            this.initialPageY = event.screenY;
+            this.initialScroll = document.body.scrollTop;
+
+            window.addEventListener('mousemove', this.handleScrollDrag);
+        });
+        window.addEventListener('mouseup', () => {
+            window.removeEventListener('mousemove', this.handleScrollDrag);
+        });
+
     }
 
     render() {
@@ -99,15 +163,29 @@ class MainPage extends Component {
                 </div>
                 <div id="main-motto">This is a great motto text. It is. Even if it's not, we'll make it great.</div>
 
-                {/*<div id="navigation-container">*/} {/*<div id="current-bar" ref={(navigationCurrentBar) => {*/}
-            {/*this.navigationCurrentBar = navigationCurrentBar*/}
-            {/*}}/>*/} {/*</div>*/}
+                <div id="navigation-container" ref={(navigationContainer) => {
+                    this.navigationContainer = navigationContainer
+                }}>
+                    <div id="current-bar" ref={(navigationCurrentBar) => {
+                        this.navigationCurrentBar = navigationCurrentBar
+                    }}/>
+                    {this.state.cardsList.map((element, index, array) => {
+                        return (
+                            <span
+                                onClick={() => this.scrollToCard(index)}
+                                key={element.title}
+                                ref={(navigationElement) => {
+                                    this.navigationElements[index] = navigationElement;
+                                }}>{element.title}</span>
+                        )
+                    })}
+                </div>
 
                 {this.state.cardsList.map((element, index, array) => {
                     return (
                         <div key={element.title} ref={(cardContainer) => {
                             this.cards[index].container = cardContainer
-                        }} className="card black">
+                        }} className="card black" id={`card-${element.title}`}>
                             <span className="card-title" ref={(cardTitle) => {
                                 this.cards[index].title = cardTitle
                             }}>{element.title}</span>
